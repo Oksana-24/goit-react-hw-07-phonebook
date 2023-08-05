16beefbeebf8c68e996672c35486a4b8b0e27736
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-08-05 19:19:23 +0200

    Create Section.jsx

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,15 @@
+import css from './Section.module.css';
+import PropTypes from 'prop-types';
+
+export const Section = ({ title, children }) => {
+  return (
+    <section className={css.sectionContainer}>
+      <h2 className={css.sectionTitle}>{title}</h2>
+      {children}
+    </section>
+  );
+};
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
