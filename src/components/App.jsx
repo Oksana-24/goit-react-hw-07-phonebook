40e32b59bd1405245eabef6c51b36dc7ec10e04e
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-08-05 21:33:48 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,19 @@
 import { useSelector } from 'react-redux';
-import { getContacts } from 'redux/selector';
+import { getContacts, selectIsLoading, selectError } from 'redux/selector';
 
 import { ContactList } from './ContactList/ContactList';
 import { Section } from './Section/Section';
 import { Filter } from './Filter/Filter';
 import { ContactForm } from './ContactForm/ContactForm';
+import { Loader } from './Loader/Loader';
 
 import css from './App.module.css';
 
 export const App = () => {
   const contacts = useSelector(getContacts);
+
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   return (
     <div className={css.appContainer}>
@@ -20,6 +24,7 @@ export const App = () => {
       <Section title="Contacts">
         {contacts.length !== 0 && <Filter />}
         <ContactList />
+        {isLoading && !error && <Loader />}
       </Section>
     </div>
   );
