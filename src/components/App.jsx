8da151831841be25cb970e8f175bfa83a07eb869
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-08-05 19:19:05 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { useSelector } from 'react-redux';
+import { getContacts } from 'redux/selector';
+
+import { ContactList } from './ContactList/ContactList';
+import { Section } from './Section/Section';
+import { Filter } from './Filter/Filter';
+import { ContactForm } from './ContactForm/ContactForm';
+
+import css from './App.module.css';
+
 export const App = () => {
+  const contacts = useSelector(getContacts);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.appContainer}>
+      <Section title="Phonebook">
+        <ContactForm />
+      </Section>
+
+      <Section title="Contacts">
+        {contacts.length !== 0 && <Filter />}
+        <ContactList />
+      </Section>
     </div>
   );
 };
