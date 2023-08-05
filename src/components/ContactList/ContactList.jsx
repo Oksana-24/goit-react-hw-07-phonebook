ad5ad77c461dad223f6f25f0141ff82ed2f3c809
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-08-05 21:34:07 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,27 +1,29 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { getContacts, getFilter } from 'redux/selector';
-import { deleteContact } from 'redux/contactSlice';
+import { useRef, useEffect } from 'react';
+import { deleteContact, fetchContacts } from 'redux/operations';
+import { selectAllContacts } from 'redux/selector';
 import PropTypes from 'prop-types';
-
 import css from './ContactList.module.css';
 
-const getFilteredContacts = (contacts, filter) =>
-  contacts.filter(({ name }) =>
-    name.toLowerCase().includes(filter.toLowerCase())
-  );
-
 export const ContactList = () => {
+  const isFirstRender = useRef(true);
   const dispatch = useDispatch();
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
+  const contacts = useSelector(selectAllContacts);
 
-  const filteredContacts = getFilteredContacts(contacts, filter);
+  useEffect(() => {
+    if (isFirstRender.current) {
+      dispatch(fetchContacts());
+    }
+    isFirstRender.current = false;
+    return;
+  }, [dispatch]);
+
   return (
     <ul className={css.contactList}>
-      {filteredContacts.map(({ id, name, number }) => (
+      {contacts.map(({ id, name, phone }) => (
         <li key={id} className={css.contactItem}>
           <span className={css.marker}></span>
-          {name}: {number}
+          {name}: {phone}
           <button
             className={css.contactButton}
             type="button"
@@ -40,7 +42,7 @@ ContactList.propTypes = {
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
+      phone: PropTypes.string.isRequired,
     }).isRequired
   ).isRequired,
   filter: PropTypes.string,
